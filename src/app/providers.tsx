ac9d82b2c1d6c6548a8faf6078b312e2f283e584
Sinashaygan <sinashaygan23041385@gmail.com>
2026-08-18 20:37:00 +0330

    feat: add the project providers

---
+++ src/app/providers.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { ReactNode } from "react";
+
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "../store";
+import { theme } from "../theme/theme";
+
+
+interface AppProvidersProps {
+  children: ReactNode;
+}
+
+export function AppProviders({ children }: AppProvidersProps) {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          {children}
+        </ThemeProvider>
+      </PersistGate>
+    </Provider>
+  );
+}
