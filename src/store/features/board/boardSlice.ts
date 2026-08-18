05cefb5f2eb9309079da634a885f38b4c2553e29
Sinashaygan <sinashaygan23041385@gmail.com>
2026-08-18 15:14:43 +0330

    feat: add board Slice

--- src/store/features/board/boardSlice.ts
+++ src/store/features/board/boardSlice.ts
@@ -1,24 +1,26 @@
+import { mockBoard, mockCards, mockColumns } from "@/src/features/board/mockData";
+import { Card, Column } from "@/src/shared/types/type";
 import {
   createEntityAdapter,
   createSlice,
   PayloadAction,
 } from "@reduxjs/toolkit";
 
-const tasksAdapter = createEntityAdapter<Task>();
+const cardAdapter = createEntityAdapter<Card>();
 const columnsAdapter = createEntityAdapter<Column>();
 
 const initialState = {
-  tasks: tasksAdapter.getInitialState(),
-  columns: columnsAdapter.getInitialState(),
-  boardInfo: { id: "b1", title: "Main Project Board" },
+  info: mockBoard,
+  tasks: cardAdapter.setAll(cardAdapter.getInitialState(), mockCards),
+  columns: columnsAdapter.setAll(columnsAdapter.getInitialState() , mockColumns)
 };
 
 const boardSlice = createSlice({
   name: "board",
   initialState,
   reducers: {
-    moveTask: (state, action) => {
-      /* ... logic ... */
-    },
+    
   },
 });
+
+export default boardSlice.reducer
