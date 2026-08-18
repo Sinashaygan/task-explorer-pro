2b934a924e1371695ba58dc8371003a473859e53
Sinashaygan <sinashaygan23041385@gmail.com>
2026-08-18 22:04:16 +0330

    feat: add the cards of columns

--- src/features/columns/ui/BoardColumn.tsx
+++ src/features/columns/ui/BoardColumn.tsx
@@ -1,7 +1,15 @@
+import { selectCardsByColumnId } from "@/src/entities/card/model/selectors";
 import { Column } from "@/src/shared/types/normalized";
+import { useAppDispatch, useAppSelector } from "@/src/store/hook";
 
 interface BoardColumnProps {
   column: Column;
 }
 
-export function BoardColumn({ column }: BoardColumnProps) {}
+export function BoardColumn({ column }: BoardColumnProps) {
+    const dispatch = useAppDispatch()
+
+    const cards = useAppSelector((state) =>
+      selectCardsByColumnId(state, column.id),
+    );
+}
