b42e6a7d407dc37dde713ed4570da8f45130f6bb
Sinashaygan <sinashaygan23041385@gmail.com>
2026-08-18 20:27:51 +0330

    Refactor: make relation between store and persist

--- src/store/index.ts
+++ src/store/index.ts
@@ -23,14 +23,18 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
+
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
     }),
+
+  devTools: process.env.NODE_ENV !== "production",
 });
 
 export const persistor = persistStore(store);
+
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
