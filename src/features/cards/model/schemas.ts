8686574884f56d46829bb6621e57ece04bf27d1d
Sinashaygan <sinashaygan23041385@gmail.com>
2026-08-20 01:19:55 +0330

    feat: add the lable and new vars to schemas

--- src/features/cards/model/schemas.ts
+++ src/features/cards/model/schemas.ts
@@ -1,6 +1,17 @@
-import z from "zod";
+import { z } from "zod";
+import { Priority } from "@/src/shared/types/normalized";
 
-export const cardSchemas = z.object({
+export interface CardFormValue {
+  title: string;
+  description: string;
+  labels: string[];
+  priority: Priority;
+  assignee: string;
+  dueDate: string;
+  isArchived: boolean;
+}
+
+export const cardSchemas: z.ZodType<CardFormValue> = z.object({
   title: z
     .string()
     .trim()
@@ -10,8 +21,19 @@ export const cardSchemas = z.object({
   description: z
     .string()
     .trim()
-    .max(1000, "Card description should be less than 1000 chars")
-    .optional(),
+    .max(1000, "Card description should be less than 1000 chars"),
+  labels: z
+    .array(z.string().trim().min(1))
+    .refine(
+      (labels) =>
+        new Set(labels.map((label) => label.toLocaleLowerCase())).size ===
+        labels.length,
+      {
+        message: "Labels should be uniques",
+      },
+    ),
+  priority: z.enum(["low", "medium", "high", "urgent"]),
+  assignee: z.string().trim().max(120),
+  dueDate: z.string(),
+  isArchived: z.boolean(),
 });
-
-export type CardFormValue = z.infer<typeof cardSchemas>
