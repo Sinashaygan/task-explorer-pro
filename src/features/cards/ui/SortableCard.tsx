eba63cb499eeb8a1118dc83d0f2614055f92ceff
Sinashaygan <sinashaygan23041385@gmail.com>
2026-08-19 11:24:17 +0330

    feat: add the sortable card properties

--- src/features/cards/ui/SortableCard.tsx
+++ src/features/cards/ui/SortableCard.tsx
@@ -1,10 +1,20 @@
 import { Card } from "@/src/shared/types/normalized";
+import { useSortable } from "@dnd-kit/sortable";
 
 interface SortableCardProps {
   card: Card;
   isOverlay?: boolean;
 }
 
-export default function SortableCard({card , isOverlay}:SortableCardProps) {
+export default function SortableCard({ card, isOverlay }: SortableCardProps) {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({ id: card.id, data: { type: "Card", card } });
+
   return <div>SortableCard</div>;
 }
