03abfe2d28e31e88e21f1823c02603d9e1b33604
Sinashaygan <sinashaygan23041385@gmail.com>
2026-08-19 11:57:28 +0330

    feat: add the sensor to kanban board for distance and key board

--- src/features/board-kanban/ui/BoardKanban.tsx
+++ src/features/board-kanban/ui/BoardKanban.tsx
@@ -3,13 +3,24 @@
 import { selectCurrentBoard } from "@/src/entities/board/model/selectors";
 import { selectBoardColumns } from "@/src/entities/column/model/selectors";
 import { EmptyState } from "@/src/shared/ui/EmptyState";
-import { useAppSelector } from "@/src/store/hook";
+import { useAppDispatch, useAppSelector } from "@/src/store/hook";
 import { Box, Stack, Typography } from "@mui/material";
 import { BoardColumn } from "../../columns/ui/BoardColumn";
+import { useState } from "react";
+import { Id } from "@/src/shared/types/normalized";
+import { KeyboardSensor, PointerSensor, useSensor, useSensors } from "@dnd-kit/core";
 
 export function BoardKanban() {
+  const dispatch = useAppDispatch();
   const board = useAppSelector(selectCurrentBoard);
   const columns = useAppSelector(selectBoardColumns);
+
+  const [activeCardId, setActiveCardId] = useState<Id | null>(null);
+
+  const sensors = useSensors(
+    useSensor(PointerSensor, { activationConstraint: { distance: 5 } }),
+    useSensor(KeyboardSensor)
+  );
 
   if (!board) {
     return (
