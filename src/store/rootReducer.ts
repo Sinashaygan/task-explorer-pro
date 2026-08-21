017b45b0bd879f7aaace4868731c3555c79d97c7
Sinashaygan <sinashaygan23041385@gmail.com>
2026-08-21 15:18:36 +0330

    feat: add the undo slice to rootReducer

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -2,10 +2,12 @@ import { combineReducers } from "@reduxjs/toolkit";
 
 import boardReducer from "./slices/boardSlice";
 import uiReducer from "./slices/uiSlice";
+import undoReducer from "./slices/undoSlice";
 
 export const rootReducer = combineReducers({
   board: boardReducer,
   ui: uiReducer,
+  undo: undoReducer,
 });
 
 export type RootReducerState = ReturnType<typeof rootReducer>;
